81b8d57d47b3fe242c196ef0d15b425735642b21
Antonybsb <artbsb1@gmail.com>
2023-11-11 10:53:30 -0300

    classes de dominio iniciadas

---
+++ src/main/java/com/antony/sishelp/domain/Tecnico.java
@@ -0,0 +1,35 @@
+package com.antony.sishelp.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//@Entity
+public class Tecnico extends Pessoa {
+	
+//	private static final long serialVersionUID = 1L;
+
+//	@JsonIgnore
+//	@OneToMany(mappedBy = "tecnico")
+	private List<Chamado> chamados = new ArrayList<>();
+
+	public Tecnico() {
+		super();
+//		addPerfil(Perfil.CLIENTE);
+	}
+
+	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+//		addPerfil(Perfil.CLIENTE);
+	}
+	
+	
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+
+}
