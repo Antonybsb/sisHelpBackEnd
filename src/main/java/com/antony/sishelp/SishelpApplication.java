1c26ad9c0102efb692172ed714eab16dae2a8f71
Antonybsb <artbsb1@gmail.com>
2023-11-11 09:26:31 -0300

    Projeto Iniciado

---
+++ src/main/java/com/antony/sishelp/SishelpApplication.java
@@ -0,0 +1,13 @@
+package com.antony.sishelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SishelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SishelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antony/sishelp/SishelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antony.sishelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SishelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
